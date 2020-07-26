f15680f32f3362d41a884777882244cd6cf2fb7a
Raihan Morshed <raihanmorshed@Raihans-MacBook-Air.local>
2020-07-25 21:49:57 -0400

    Updates

---
+++ precache-manifest.6b854aafe95d402376c45baa9c56b6b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b3fff60ddf45a65b683234f73ddf09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0903660d069ea45a107f",
+    "url": "/static/css/main.bfe8349f.chunk.css"
+  },
+  {
+    "revision": "e6f6cd8086db95404770",
+    "url": "/static/js/2.d23fdd0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d23fdd0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0903660d069ea45a107f",
+    "url": "/static/js/main.709d8e2e.chunk.js"
+  },
+  {
+    "revision": "e968eeb8c247134aad12",
+    "url": "/static/js/runtime-main.bdad0aff.js"
+  }
+]);
